d96e8a52b7a0a7f99d5e675c30dbfd3508f317f2
maurotechweb2244 <mauroml2300@gmail.com>
2023-05-11 21:16:29 -0300

    modified app

--- src/main/java/com/portfolio/proyectoml/Controller/PersonaController.java
+++ src/main/java/com/portfolio/proyectoml/Controller/PersonaController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -20,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/Perfil")
+@CrossOrigin(origins = "http://localhost:4200")
 public class PersonaController {
     
     @Autowired
