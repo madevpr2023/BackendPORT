9056ea1a5658c461ffa58b874f95defc9385fe1e
madevpr2023 <mauroml2300@gmail.com>
2023-05-14 20:58:42 -0300

    Modificado

--- src/main/java/com/portfolio/proyectoml/Secure/Service/UserDetailsIm.java
+++ src/main/java/com/portfolio/proyectoml/Secure/Service/UserDetailsIm.java
@@ -3,6 +3,7 @@ package com.portfolio.proyectoml.Secure.Service;
 
 import com.portfolio.proyectoml.Secure.Entity.User;
 import com.portfolio.proyectoml.Secure.Entity.UserPrincipal;
+import com.portfolio.proyectoml.Secure.Repositorio.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -14,12 +15,12 @@ public class UserDetailsIm implements UserDetailsService{
     
     @Autowired
     
-    UserServ userv;
+    UserRepo userep;
 
     @Override
     public UserDetails loadUserByUsername(String nameUser) throws UsernameNotFoundException {
        
-        User usuario = userv.getByNameUser(nameUser).get();
+        User usuario = userep.findByNameUser(nameUser).get();
         
         return UserPrincipal.build(usuario);
     }
